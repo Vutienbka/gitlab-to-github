0b789afa80fb8526815e753142ddb30799025c32
ryohasegawa <zaqopqqq@gmail.com>
2020-03-19 07:13:41 +0700

    Rebuild css structures

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -18,4 +18,4 @@ Rails.application.config.assets.precompile += %w[color.css users/*.sass users/*.
 Rails.application.config.assets.precompile += %w[application_admin.scss application_admin.js devise.scss]
 
 Rails.application.config.assets.precompile += %w[home.scss home.js calendar.scss menu.js]
-Rails.application.config.assets.precompile += %w[newji/*.css]
+Rails.application.config.assets.precompile += %w[templates/*.css]
