c106e2e9c03a2e5883b0c6886a2b0dc99f51f0fb
ryohasegawa <zaqopqqq@gmail.com>
2020-04-01 15:05:47 +0700

    fix update profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   acts_as_paranoid
 
+  attr_accessor :update_profile
+
   PARAMS_ATTRIBUTES = [
-    :email, :password, :password_confirmation,
+    :email, :password, :password_confirmation, :update_profile,
     :accept_flg, :antiforce_flg, :access_IP, :creator, :updater,
     profile_attributes: [
       :id, :first_name, :last_name, :tel, :company_name, :logo,
@@ -15,8 +17,12 @@ class User < ApplicationRecord
   ]
 
   validates :password, confirmation: { case_sensitive: true }
-  validates :password, presence: true
+  validates :password, presence: true, unless: :check_update_profile?
   validates :email, presence: true
+
+  def check_update_profile?
+    update_profile.present?
+  end
 
   def buyer?
     type == 'Buyer'
