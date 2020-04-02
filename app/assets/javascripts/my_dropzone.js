21464338a37db98f1027e4b6832ba4cf43687d81
ryohasegawa <zaqopqqq@gmail.com>
2020-04-02 21:45:02 +0700

    Validate dropzone by jquery

--- app/assets/javascripts/my_dropzone.js
+++ app/assets/javascripts/my_dropzone.js
@@ -21,12 +21,17 @@ $('.dropzone').each(function(){
     },
     init: function() {
       var myDropzone = this;
+
       $(".continue-btn").on("click", function(e) {
+        e.preventDefault();
+        e.stopPropagation();
         if (myDropzone.getQueuedFiles().length > 0) {
-          e.preventDefault();
-          e.stopPropagation();
+          dropzone_uploaded_file(id);
+          check_dropzone_length();
           myDropzone.processQueue();
           return;
+        } else {
+          dropzone_no_file_upload(id);
         }
       });
       this.on("sendingmultiple", function(data, xhr, formData) {
@@ -34,11 +39,41 @@ $('.dropzone').each(function(){
         formData.append("item_drawing[draw_categories_attributes][" + id + "][id]", $('#item_drawing_draw_categories_attributes_' + id + '_id').val());
       });
       this.on("addedfile", function(file) {
-        $("#dropzone-alert" + id).css("display", "block");
+        dropzone_uploaded_file(id);
+        check_dropzone_length();
+      });
+      this.on("removedfile", function(file) {
+        if (myDropzone.files.length <= 0) {
+          dropzone_no_file_upload(id);
+          check_dropzone_length();
+        }
       });
       this.on("successmultiple", function(files, response) {
         window.location.href = "/users/input_items_image?item_request_id=" + item_request_id
       });
     }
   });
+});
+
+
+function dropzone_no_file_upload(id) {
+  $("#dropzone_no_file_upload" + id).css("display", "block");
+  $("#dropzone_alert" + id).css("display", "none");
+}
+
+function dropzone_uploaded_file(id) {
+  $("#dropzone_no_file_upload" + id).css("display", "none");
+  $("#dropzone_alert" + id).css("display", "block");
+}
+
+function check_dropzone_length() {
+  if($('#dropzone_alert0').is(':visible') && $('#dropzone_alert3').is(':visible')) {
+    $("#btnSubmit").removeAttr('disabled');
+  } else {
+    $("#btnSubmit").attr('disabled', 'disabled');
+  }
+}
+
+$(document).ready(function() {
+  $("#btnSubmit").attr('disabled', 'disabled');
 });
