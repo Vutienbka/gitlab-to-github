1121cd94e61b499f1de1a8e22a7a7819e93d5937
ryohasegawa <zaqopqqq@gmail.com>
2020-04-18 01:21:07 +0700

    both buyer/supplier view & display global event

--- app/controllers/buyers/calendar_controller.rb
+++ app/controllers/buyers/calendar_controller.rb
@@ -1,17 +1,17 @@
+# frozen_string_literal: true
+
 class Buyers::CalendarController < Buyers::BaseController
   before_action :redirect_to_profile
 
   def index
     @date = Date.current
     show_calendar
-    @user_calendars = UserCalendar.where(creator: current_user.id).group_by { |event| event.occur_date.to_date }
-    
-    
+    @user_calendars = UserCalendar.where(creator: current_user.id).or(UserCalendar.where(user_id: [current_user.id, 0])).group_by { |event| event.occur_date.to_date }
   end
-
+  ``
   def show_calendar
     @date = params[:start_date]&.to_date || Date.current
     start_date = @date.beginning_of_month
-    @user_calendars = UserCalendar.where(creator: current_user.id).group_by { |event| event.occur_date.to_date }
+    @user_calendars = UserCalendar.where(creator: current_user.id).or(UserCalendar.where(user_id: [current_user.id, 0])).group_by { |event| event.occur_date.to_date }
   end
 end
