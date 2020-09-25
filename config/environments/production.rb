726b1ac613d209768347089cb56c349285191812
ryohasegawa <zaqopqqq@gmail.com>
2020-09-26 00:20:37 +0700

    Add secret key base to .env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.secret_key_base = ENV['SECRET_KEY_BASE']
 
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -23,11 +24,11 @@ Rails.application.configure do
   config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
-  config.assets.js_compressor = Uglifier.new(harmony: true)
+  # config.assets.js_compressor = Uglifier.new(harmony: true)
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  config.assets.compile = true
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
