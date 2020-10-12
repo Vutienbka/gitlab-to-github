292c68f5f6a25b39db486bf81b5c3afda620a1cb
ryohasegawa <zaqopqqq@gmail.com>
2020-10-12 12:19:13 +0700

    fix bugs search catalogs

--- app/controllers/buyers/searchs_controller.rb
+++ app/controllers/buyers/searchs_controller.rb
@@ -6,18 +6,17 @@ class Buyers::SearchsController < Buyers::BaseController
     else
       item_sku_id = ItemRequest.joins(:item_info).where('item_info.SKU like ?', "%#{params[:q][:status_cont]}%").ids
       item_name_id = ItemRequest.joins(:item_info).where('item_info.name like ?', "%#{params[:q][:status_cont]}%").ids
-      catalog_id = ItemRequest.includes(:catalog).where(catalog_id: Catalog.where("name like ? ", "%#{params[:q][:status_cont]}%").ids).ids
+      catalog_id = ItemRequest.includes(:catalog).where(catalog_id: Catalog.where('name like ? ', "%#{params[:q][:status_cont]}%").ids).ids
       supplier_id = ItemRequest.where(supplier_id: Profile.where('first_name like ? ', "%#{params[:q][:status_cont]}%").ids).ids
       ids = item_sku_id + item_name_id + catalog_id + supplier_id
-      @item_requests = ItemRequest.where(id: ids).includes([:item_info])
+      @item_requests = ItemRequest.where(id: ids, status: 7).where.not(catalog_id: nil).includes([:item_info])
 
       @q = @item_requests.ransack.result.page(params[:page]).per 10
       render :search unless @item_requests.nil?
     end
   end
 
-  def search
-  end
+  def search; end
 
   def list_auto
     # @item_info = ItemInfo.ransack(params[:q])
@@ -29,19 +28,17 @@ class Buyers::SearchsController < Buyers::BaseController
     else
       item_sku_id = ItemRequest.joins(:item_info).where('item_info.SKU like ?', "%#{params[:q]}%").ids
       item_name_id = ItemRequest.joins(:item_info).where('item_info.name like ?', "%#{params[:q]}%").ids
-      catalog_id = ItemRequest.includes(:catalog).where(catalog_id: Catalog.where("name like ? ", "%#{params[:q]}%").ids).ids
+      catalog_id = ItemRequest.includes(:catalog).where(catalog_id: Catalog.where('name like ? ', "%#{params[:q]}%").ids).ids
       supplier_id = ItemRequest.where(supplier_id: Profile.where('first_name like ? ', "%#{params[:q]}%").ids).ids
       ids = item_sku_id + item_name_id + catalog_id + supplier_id
       @item_requests = ItemRequest.where(id: ids).includes([:item_info])
-      q= []
+      q = []
       # @q = @item_requests.ransack.result.page(params[:page]).per 10
       @item_requests.each do |it|
         q << it.item_info
       end
     end
-    if @q.blank?
-      @q = []
-    end
+    @q = [] if @q.blank?
     render json: q
   end
 end
